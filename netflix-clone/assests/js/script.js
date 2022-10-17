74896c01366e0b32665d2292688465d40f18253b
Biyinzika-Joshua-J <joshuabiyinzika22@gmail.com>
2022-10-17 17:27:46 +0100

    worked on faqs

--- netflix-clone/assests/js/script.js
+++ netflix-clone/assests/js/script.js
@@ -1,12 +1,14 @@
 //faq toggle feature
-const questionBody = document.querySelector('.qn');
-const contentBody = document.querySelector('.content');
-const cross = document.querySelector('.svg-icon-thin-x ');
+const questionBody = document.querySelectorAll(".qn");
+const contentBody = document.querySelectorAll(".content");
+const cross = document.querySelectorAll(".svg-icon-thin-x ");
 
-const toggleQuestionBody = function(e){
-    contentBody.classList.toggle('hide');
-}
+// cross.addEventListener('click', toggleQuestionBody);
 
-questionBody.addEventListener('click', toggleQuestionBody);
-cross.addEventListener('click', toggleQuestionBody);
-
+questionBody.forEach((question, index) => {
+  question.addEventListener("click", function (e) {
+    cross[index].classList.toggle("svg-closed");
+    contentBody[index].classList.toggle("hide");
+    contentBody[index].classList.toggle("is-open");
+  });
+});
