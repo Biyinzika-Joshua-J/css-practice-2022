1220d680753df805c77832013493cccbbb2b466d
Biyinzika-Joshua-J <joshuabiyinzika22@gmail.com>
2022-10-18 14:11:58 +0100

    all sections finsihed

--- netflix-clone/assests/js/script.js
+++ netflix-clone/assests/js/script.js
@@ -3,12 +3,9 @@ const questionBody = document.querySelectorAll(".qn");
 const contentBody = document.querySelectorAll(".content");
 const cross = document.querySelectorAll(".svg-icon-thin-x ");
 
-// cross.addEventListener('click', toggleQuestionBody);
-
 questionBody.forEach((question, index) => {
   question.addEventListener("click", function (e) {
     cross[index].classList.toggle("svg-closed");
     contentBody[index].classList.toggle("hide");
-    contentBody[index].classList.toggle("is-open");
   });
 });
