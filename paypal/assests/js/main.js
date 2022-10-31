21fd95eb32408620ee032a3a2fae9ede0d146901
Biyinzika-Joshua-J <joshuabiyinzika22@gmail.com>
2022-10-31 21:03:58 +0100

    almost finishing paypal navbar

---
+++ paypal/assests/js/main.js
@@ -0,0 +1,17 @@
+const arrow = document.querySelector('.arrow');
+const navBarItems = document.querySelector('.navbar-items');
+
+const openNavBarItems = function(e){
+   navBarItems.style.height = 'auto';
+   console.log(e)
+}
+
+const closeNavbarItems = function(e){
+    navBarItems.style.height = 0;
+}
+
+arrow.addEventListener('mouseover', openNavBarItems);
+//arrow.addEventListener('mouseleave', closeNavbarItems);
+
+navBarItems.addEventListener('mouseover', openNavBarItems);
+navBarItems.addEventListener('mouseleave', closeNavbarItems);
